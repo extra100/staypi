eb9e5396897520b192a028476880dbd47f266623
bismillah212-art <bismillah212-art@gmail.com>
2024-12-02 15:17:22 +0700

    egaerhsjs6rjs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,21 +5,10 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   base: '/',
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // Sesuaikan base path jika deploy ke subfolder
+  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory
-    chunkSizeWarningLimit: 1000, // Sesuaikan jika ada peringatan ukuran file
-  },
 })
